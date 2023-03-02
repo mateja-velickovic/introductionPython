e9ee4a778194a6eba71e43f19b4efdbeecf1b910
mateja-velickovic <mateja.velickovic@eduvaud.ch>
2023-03-02 22:06:35 +0100

    Introduction au Python
    Déclaration de variables, Création de listes (ajout/suppression/tri d'éléments)

---
+++ introductionPython/python_1/python_1.py
@@ -0,0 +1,31 @@
+# Déclarer une liste
+reseauxSociaux = ["Instagram", "Tiktok"]
+
+# Ajouter un élément 
+reseauxSociaux.append("Snapchat")
+
+# Enlever un élément  
+reseauxSociaux.remove("Instagram")
+
+# Afficher la liste dans la console
+print (reseauxSociaux)
+
+# Connaître la longueur de la liste
+print(len(reseauxSociaux))
+
+
+
+# Déclarer une variable
+instagram = ("Instagram")
+
+# Afficher la variable5
+print(instagram)
+# Connaître la longueur du string
+print(len(instagram))
+
+# Afficher la valeur dans le 3ème index de la liste [commence par 0]
+print (instagram[3])
+
+# Trier les éléments par ordre alphabétique
+reseauxSociaux.sort()
+print(reseauxSociaux)
