2833fe79bf1a01e453a9053a365534ec95d5ee57
mateja-velickovic <mateja.velickovic@eduvaud.ch>
2023-03-03 14:26:35 +0100

    Création de dictionnaire
    Création, suppression et affichage d'un dictionnaire

--- introductionPython/python_1/python_1.py
+++ introductionPython/python_1/python_1.py
@@ -20,6 +20,7 @@ instagram = ("Instagram")
 
 # Afficher la variable5
 print(instagram)
+
 # Connaître la longueur du string
 print(len(instagram))
 
@@ -29,3 +30,17 @@ print (instagram[3])
 # Trier les éléments par ordre alphabétique
 reseauxSociaux.sort()
 print(reseauxSociaux)
+
+# Créer/afficher/supprimer un dictionnaire
+chaussure = {
+    "taille" : 42,
+    "marque" : "Nike",
+    "race" : "berger-allemand"
+}
+
+del chaussure["race"]
+
+print (chaussure)
+
+# Supprimer une paire clé-valeur
+del infos_labradoodle["origine"]
